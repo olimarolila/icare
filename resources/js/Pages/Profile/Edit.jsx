926ac685fe7855c162d01b5c106094f272366ea9
olimarolila <olimardominic.olila.cics@ust.edu.ph>
2025-12-03 14:40:55 +0800

    remove the unused components

--- resources/js/Pages/Profile/Edit.jsx
+++ resources/js/Pages/Profile/Edit.jsx
@@ -3,7 +3,20 @@ import { Head } from "@inertiajs/react";
 import DeleteUserForm from "./Partials/DeleteUserForm";
 import UpdatePasswordForm from "./Partials/UpdatePasswordForm";
 import UpdateProfileInformationForm from "./Partials/UpdateProfileInformationForm";
-import PrimaryButton from "@/Components/PrimaryButton";
+
+const PrimaryButton = ({ className = '', disabled, children, ...props }) => (
+    <button
+        {...props}
+        className={
+            `inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 ${
+                disabled && 'opacity-25'
+            } ` + className
+        }
+        disabled={disabled}
+    >
+        {children}
+    </button>
+);
 
 export default function Edit({ mustVerifyEmail, status }) {
     return (
